fa441f764b9aae053da378085f5cd610a0b8cf23
Danilo <danilodevsp@gmail.com>
2025-07-03 13:31:01 -0300

    Deploy da api

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3333",
+  origin: "https://devbnb-front.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
