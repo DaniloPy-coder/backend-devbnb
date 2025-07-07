ded8aec2b4af89d780408fc14f0c185989a06932
Danilo <danilodevsp@gmail.com>
2025-07-07 15:42:41 -0300

    Deploy da api

--- src/services/place/ListAllPlacesService.ts
+++ src/services/place/ListAllPlacesService.ts
@@ -2,24 +2,15 @@ import { prismaClient } from "../../prisma";
 
 export class ListAllPlacesService {
     async execute() {
-        const hoje = new Date(); // usa objeto Date, não string
+        const hoje = new Date().toISOString().split("T")[0];
 
         const places = await prismaClient.place.findMany({
             where: {
                 bookings: {
                     none: {
-                        AND: [
-                            {
-                                checkin: {
-                                    lte: hoje,
-                                },
-                            },
-                            {
-                                checkout: {
-                                    gte: hoje,
-                                },
-                            },
-                        ],
+                        checkout: {
+                            gte: hoje,
+                        },
                     },
                 },
             },
